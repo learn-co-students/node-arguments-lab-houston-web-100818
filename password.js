45bea38c6efb67fe21778ba1f82065e25f5da15e
barbaradecares <barbaradecares@hotmail.com>
2019-01-30 17:23:35 -0900

    done

--- password.js
+++ password.js
@@ -1,1 +1,23 @@
-// code solution here
+if (process.argv.length !== 4) {
+  console.log("Wrong numbers of arguments.");
+} else {
+  process.argv.shift();
+  process.argv.shift(); //deletes first 2 default arguments
+
+  const amount = parseInt(process.argv[1], 10);
+
+  for (let i = 0; i < amount; i++) {
+    generatePassword(parseInt(process.argv[0], 10));
+  }
+}
+
+function generatePassword(length) {
+  var password = "";
+  var possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < length; i++)
+    password += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  console.log(password);
+}
